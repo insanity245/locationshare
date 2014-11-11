c21195146dc44620a5b68d0d5223c350878c6d92
Sourav Basu <devels17@gmail.com>
2014-11-11 22:21:21 +0530

    Basic stuff

--- app/views.py
+++ app/views.py
@@ -1,7 +1,6 @@
 from app import app
 from app import methods
 from flask import render_template
-from app import forms
 
 # default route
 @app.route('/')
@@ -14,16 +13,3 @@ def test():
 	user = {'nickname': 'rmad'}  # fake user
 	return render_template('test.html', title='Flask Home',user=user)
 
-@app.route('/share', methods=['GET', 'POST'])
-def processValidation():
-	form = forms.sharestatusform(request.form)
-	if request.method == 'POST' and form.validate():
-		user = User(form.name.data, form.status.data)
-		flash('Thanks for registering')
-		return redirect('/')
-	return render_template('status.html', form=form)
-
-	# geohash test route
-@app.route('/geohash')
-def geohashtest():
-	return methods.geohash()
